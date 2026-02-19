e2d14fc6e6770b973ef9ba728e0c7500045d610d
AlikoGrace <gracealiko09@gmail.com>
2026-02-19 22:14:23 +0000

    execute first tool call and print resutlts

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import json
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -11,6 +12,9 @@ load_dotenv()
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v1")
+USE_FREE = os.getenv("USE_FREE", "false").lower() == "true"
+REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 
 # If USE_FREE=true, use a free OpenRouter model
 USE_FREE = os.getenv("USE_FREE", "false").lower() == "true"
@@ -54,13 +58,34 @@ def main():
 
     if not chat.choices or len(chat.choices) == 0:
         raise RuntimeError("no choices in response")
+    
+    msg=chat.choices[0]
+     # for debugging purposes
+    print("TOOL CALLS:", msg.tool_calls, file=sys.stderr)
+    if msg.tools_calls:
+        tool_call=msg.tool_call[0]
+        tool_name=tool_call.function.name
+        tool_args=json.loads(tool_call.function.arguments)
 
+
+        # to executing toolcall\
+        if tool_name=="read":
+            path=tool_args['path']
+            
+            with open(path, "r", encoding="utf-8") as f:
+             results=f.read()
+
+             print(results, end="")
+        else:
+            raise RuntimeError(f"Unknowwn tool: {tool_name}")     
+        
+    else:
+        print(chat.choices[0].message.content)         
+ 
     
     print(f"[Using model: {model}]", file=sys.stderr)
 
-    # TODO: Uncomment the following line to pass the first stage 
-    print(chat.choices[0].message.content)
-
+    
 
 if __name__ == "__main__":
     main()
